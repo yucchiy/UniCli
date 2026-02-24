04fd7a03b379e9d9f735f2c2e368054b4bdd7b45
Yuichiro MUKAI <y.iky917@gmail.com>
2026-02-24 12:51:48 +0900

    Skip link.xml generation when UniCli.Remote is not included in build
    RemoteLinkerProcessor unconditionally generated a link.xml preserving
    the UniCli.Remote assembly, even in Release builds where the assembly
    is not compiled due to asmdef defineConstraints. Now check that the
    build is a Development Build and UNICLI_REMOTE is defined before
    generating the linker preservation entry.

--- src/UniCli.Unity/Packages/com.yucchiy.unicli-server/Editor/Build/RemoteLinkerProcessor.cs
+++ src/UniCli.Unity/Packages/com.yucchiy.unicli-server/Editor/Build/RemoteLinkerProcessor.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEditor.UnityLinker;
@@ -11,9 +13,27 @@ namespace UniCli.Server.Editor.Build
 
         public string GenerateAdditionalLinkXmlFile(BuildReport report, UnityLinkerBuildPipelineData data)
         {
+            if (!IsRemoteAssemblyIncluded(report))
+                return null;
+
             var path = Path.Combine(data.inputDirectory, "UniCli.Remote.link.xml");
             File.WriteAllText(path, "<linker>\n  <assembly fullname=\"UniCli.Remote\" preserve=\"all\" />\n</linker>\n");
             return path;
+        }
+
+        private static bool IsRemoteAssemblyIncluded(BuildReport report)
+        {
+            // UniCli.Remote asmdef defineConstraints:
+            //   "DEVELOPMENT_BUILD || UNITY_EDITOR"
+            //   "UNICLI_REMOTE || UNITY_EDITOR"
+            // In player builds UNITY_EDITOR is not defined,
+            // so both DEVELOPMENT_BUILD and UNICLI_REMOTE must be present.
+            if ((report.summary.options & BuildOptions.Development) == 0)
+                return false;
+
+            var targetGroup = BuildPipeline.GetBuildTargetGroup(report.summary.platform);
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup);
+            return Array.IndexOf(defines.Split(';'), "UNICLI_REMOTE") >= 0;
         }
     }
 }
